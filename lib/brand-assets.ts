755eee779359edf92c3d469450cdae9317065c1c
Volker Willems <dev@volkerwillems.nl>
2026-07-25 14:29:43 +0200

    Centralize Earth Spas brand assets

---
+++ lib/brand-assets.ts
@@ -0,0 +1,12 @@
+export const brandAssets = {
+  fullLogo: "/logos/earthspas-full.svg",
+  goldLogo: "/logos/logo-full-gold.png",
+  icon: "/logos/earthspas-icon.svg",
+  iconBlue: "/logos/earthspas-icon-blue.png",
+  favicon: "/logos/favicons/favicon.ico",
+  favicon16: "/logos/favicons/favicon-16x16.png",
+  favicon32: "/logos/favicons/favicon-32x32.png",
+  appleTouchIcon: "/logos/app-icons/apple-touch-icon.png",
+  androidIcon192: "/logos/app-icons/android-chrome-192x192.png",
+  androidIcon512: "/logos/app-icons/android-chrome-512x512.png",
+} as const;
