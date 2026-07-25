7fa04f2fa7431d7c5d5cb30222f1724655820b29
Volker Willems <dev@volkerwillems.nl>
2026-07-25 15:47:15 +0200

    Export reusable Motion flow components

---
+++ components/motion/index.ts
@@ -0,0 +1,9 @@
+export { AgentFlowShowcase } from "./agent-flow-showcase";
+export {
+  AnimatedConnectorLayer,
+  FlowNode,
+  FlowStage,
+  type FlowConnector,
+  type FlowIcon,
+  type FlowMobileStep,
+} from "./flow-kit";
