64144c483f602fd0618ea2479e709861caae42c7
Volker Willems <dev@volkerwillems.nl>
2026-07-25 17:05:37 +0200

    Add shared SiteFooter component

---
+++ components/layout/site-footer.tsx
@@ -0,0 +1,7 @@
+export function SiteFooter() {
+  return (
+    <footer className="global-plan-footer">
+      Earth Spas digitale keuzehulp · pagina-inhoud, keuzes en actievoortgang worden centraal uit dezelfde instellingen bijgewerkt
+    </footer>
+  );
+}
